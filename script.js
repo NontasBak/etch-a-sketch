881acb7717848ba364773094f00181cace035195
NontasBak <nontasbak@gmail.com>
2023-07-26 21:54:20 +0300

    Center everything and keep container size constant

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let dimension = 16;
-const body = document.querySelector("body");
+const bigContainer = document.querySelector(".big-container");
 let container = document.createElement("div");
 container.classList.add("container");
 
 createBoard(dimension);
-body.appendChild(container);
+bigContainer.appendChild(container);
 
-const button = document.createElement("button");
+const button = document.querySelector("button");
+button.style.margin = "auto";
+button.style.width = "150px"
+button.style.height = "100px"
+button.style.fontSize = "25px"
+button.textContent = "Change grid";
+
 button.addEventListener("click", () => {
     dimension = prompt("Enter the number of rows/columns.")
     if(dimension < 2 || dimension > 100)
@@ -18,9 +24,6 @@ button.addEventListener("click", () => {
     container.textContent = "";
     createBoard(dimension);
 })
-button.textContent = "Change grid";
-body.appendChild(button);
-
 
 
 function createBoard(dimension) {
@@ -30,7 +33,9 @@ function createBoard(dimension) {
 
         for(let j = 0; j < dimension; j++)
         {
-            let square = document.createElement("div")
+            let square = document.createElement("div");
+            size = 500 / dimension;
+            square.style.cssText = `width: ${size}px; height: ${size}px;`;
             square.classList.add("square");
 
             square.addEventListener("mouseover", () => {
